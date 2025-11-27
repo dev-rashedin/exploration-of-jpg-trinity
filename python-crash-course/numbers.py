988a3685970bfa2dc5c72967c799f99082d6bf43
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-27 19:14:36 +0600

    numbers explored

---
+++ python-crash-course/numbers.py
@@ -0,0 +1,15 @@
+x = 1
+x = 1.1
+x = 1 + 2j # a + bi --> complex number
+
+print('addition', 10 + 3)
+print('subtraction', 10 - 3)
+print('multiplication', 10 * 3)
+print('division', 10 / 3)
+print('floor division', 10 // 3)
+print('modulus', 10 % 3)
+print('exponent', 10 ** 3)
+
+x = 10
+x = x + 3
+x += 3
---
+++ python-crash-course/working-with-numbers.py
@@ -0,0 +1,12 @@
+import math;
+
+print('round',round(2.51)) # more than 0.5 it will round up
+print('round', round(2.5)) #  5 or less than 0.5 it will round down
+
+print('abs', abs(-2))
+
+print('max', max(1,2,3,4,5))
+print('min', min(1,2,3,4,5))
+
+
+print('ceil',math.ceil(2.1))
