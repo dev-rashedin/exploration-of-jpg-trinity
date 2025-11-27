36b2404ea982f403c4d90aa71820a8f165fe9ee6
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-27 15:08:06 +0600

    basics and variables explored

---
+++ python-crash-course/app.py
@@ -0,0 +1,14 @@
+# print('Hello World')
+# print('*' * 10)
+
+# print("Hello World")
+
+# 2 + 3
+
+x = 1
+y = 2
+unit_price = 3
+
+total_price = x * y * unit_price
+
+print(total_price)
---
+++ python-crash-course/variable.py
@@ -0,0 +1,14 @@
+student_count = 1000  # number (integer)
+rating = 4.99  # number (float)
+is_published = True  # boolean (capitalized)
+course_name = "Python for Beginners"  # string
+
+
+message = """
+Hello World
+This is a message
+"""
+
+
+print(message)  
+
