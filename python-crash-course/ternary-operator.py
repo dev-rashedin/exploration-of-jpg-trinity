6adc19cd14b314fc94fff34cddf6d82d21690105
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-27 23:24:49 +0600

    conditional operator and ternary operator explored

---
+++ python-crash-course/ternary-operator.py
@@ -0,0 +1,9 @@
+age = 12
+if age >= 18:
+   message="You are an adult"
+else:
+   message="You are not an adult"
+
+message = 'You are an adult' if age>=18 else 'You are not an adult'
+
+print(message)
