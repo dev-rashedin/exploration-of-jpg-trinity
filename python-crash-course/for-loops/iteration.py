2d4419ba3dd541aee4d40f675b1a6a160a32b5c1
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-28 11:12:28 +0600

    for loops explored

---
+++ python-crash-course/for-loops/iteration.py
@@ -0,0 +1,14 @@
+# print(type(5))
+# print(type(range(5)))
+
+# range object is iterable and we can use it inside for loop
+
+
+# for x in 'Python':
+#   print(x)
+
+# for x in [1, 2, 3, 4]:
+#   print(x)
+
+for item in shopping_cart:
+  print(item)
