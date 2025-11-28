370de29a8f4542b3ca584aaf88a5ef8688ff125b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-28 09:57:17 +0600

    logical-operator explored

---
+++ python-crash-course/logical-operator.py
@@ -0,0 +1,25 @@
+high_income = True
+good_credit = False
+student = False
+
+# if high_income and good_credit:
+#   print("Eligible for loan")
+# else:
+#   print("Not eligible for loan")
+
+
+# if high_income or good_credit:
+#   print("Eligible for loan")
+# else:
+#   print("Not eligible for loan")
+
+# if not student: 
+#   print("Eligible for loan")
+# else:
+#   print("Not eligible for loan")
+
+
+if (high_income or good_credit) and not student:
+  print("Eligible for loan")
+else:
+  print("Not eligible for loan")
