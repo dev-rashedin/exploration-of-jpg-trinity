b5bb9db408088fff26b55441af02753d951c1c29
Rashedin-063 <rashedinislam.06@gmail.com>
2025-11-28 10:08:17 +0600

    chaining-comparison-operator explored

---
+++ python-crash-course/chaining-comparison-operator.py
@@ -0,0 +1,13 @@
+# age should be between 18 and 65
+
+age = 22
+
+# if age >-18 and age <65:
+#   print("You are an adult")
+# else:
+#   print("You are not an adult")
+
+if 18 <= age < 65:
+    print("You are an adult")
+else:
+    print("You are not an adult")
--- python-crash-course/logical-operator.py
+++ python-crash-course/logical-operator.py
@@ -1,5 +1,5 @@
 high_income = True
-good_credit = False
+good_credit = True
 student = False
 
 # if high_income and good_credit:
@@ -19,7 +19,7 @@ student = False
 #   print("Not eligible for loan")
 
 
-if (high_income or good_credit) and not student:
+if (high_income and good_credit) and not student:
   print("Eligible for loan")
 else:
   print("Not eligible for loan")
