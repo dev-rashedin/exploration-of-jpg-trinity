dfd5b801110d3985446a9e15f6212695f02d2bac
Rashedin-063 <rashedinislam.06@gmail.com>
2025-12-08 22:54:20 +0600

    lists in python explored

--- python/python-full-course/09.lists-sets-tuples/lists.py
+++ python/python-full-course/09.lists-sets-tuples/lists.py
@@ -27,4 +27,25 @@ fruits = ['apple', 'banana', 'cherry', 'orange']
 # print(fruits)
 
 fruits.append('mango')  # add item to the end of the list
+
+fruits.remove('apple') # remove item
+
+fruits.insert(1, 'kiwi') # insert item at preferred index
+
+fruits.insert(0, 'apple')
+
+fruits.sort() # sort the list in the alphabetical order
+
+fruits.reverse()  # reverse the list
+
+# fruits.clear() # clear the list
+
+appleIndex = fruits.index('apple') # returns the index of the item
+
+print(appleIndex)
+
+bananaCount = fruits.count('banana') # returns the number of times the item appears in the list
+
+print(bananaCount)
+
 print(fruits)
