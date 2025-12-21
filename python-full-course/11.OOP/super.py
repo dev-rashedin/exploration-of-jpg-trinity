f19de544e58a75eba7d2b52573481a547e304e45
Rashedin-063 <rashedinislam.06@gmail.com>
2025-12-21 21:39:26 +0600

    super function explored

--- python-full-course/11.OOP/super.py
+++ python-full-course/11.OOP/super.py
@@ -8,15 +8,26 @@ class Shape:
     self.color = color
     self.filled = filled
 
+  def describe(self):
+    print(f"It is {self.color} and {'filled' if self.filled else 'not filled'}")
+
 class Circle(Shape):
   def __init__(self, color, filled, radius):
     super().__init__(color, filled)
     self.radius = radius
 
+  def describe(self):
+    super().describe()
+    print(f"It is a circle with an area of {3.14 * self.radius ** 2}cm²")
+
 class Square:
   def __init__(self, color, filled, width):
     super().__init__(color, filled)
     self.width = width
+
+  def describe(self):
+    super().describe()
+    print(f"It is a square with an area of {self.width ** 2}cm²")
 
 class Triangle:
   def __init__(self, color, filled, base, height):
@@ -24,3 +35,15 @@ class Triangle:
     self.base = base
     self.height = height
 
+  def describe(self):
+    super().describe()
+    print(f"It is a triangle with an area of {0.5 * self.base * self.height}cm²")
+
+
+
+circle = Circle(color='red', filled= True, radius=5)
+
+# print(f"Circle color: {circle.color}, filled: {circle.filled}, radius: {circle.radius}")
+
+
+circle.describe()
